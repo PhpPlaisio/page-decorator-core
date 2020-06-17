a2b8d31449c82d9836306e275d910920fc654090
Paul Water <p.r.water@setbased.nl>
2020-06-17 18:10:40 +0200

    Add icons.

--- src/CorePageDecorator.php
+++ src/CorePageDecorator.php
@@ -47,6 +47,7 @@ class CorePageDecorator extends PlaisioObject implements PageDecorator
     $this->nub->assets->cssAppendSource('plaisio/page-decorator-core/input-table.css');
     $this->nub->assets->cssAppendSource('plaisio/page-decorator-core/input-table-small.css');
     $this->nub->assets->cssAppendSource('plaisio/page-decorator-core/button.css');
+    $this->nub->assets->cssAppendSource('plaisio/page-decorator-core/icons-small.css');
 
     $this->nub->assets->setPageTitle($this->nub->pageInfo['ptb_title'].
                                      ' - '.
@@ -59,9 +60,9 @@ class CorePageDecorator extends PlaisioObject implements PageDecorator
    */
   private function echoAdminMenu(): void
   {
-    echo '<div class="grid-main-menu">';
+    echo '<div class="grid-admin-menu">';
 
-    echo '<div class="main-menu-icon">';
+    echo '<div class="admin-menu-icon">';
     echo '<div class="menu-bar1"></div>';
     echo '<div class="menu-bar2"></div>';
     echo '<div class="menu-bar3"></div>';
