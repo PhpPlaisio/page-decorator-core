51a2bed830288fb62ded3e5a906c580a9499254a
Paul Water <p.r.water@setbased.nl>
2020-07-10 13:37:52 +0200

    Migrate to TypeScript.

---
+++ www/js/Plaisio/PageDecorator/CorePageDecorator.main.ts
@@ -0,0 +1,12 @@
+// @ts-ignore
+requirejs.config({
+  baseUrl: '/js',
+  paths: {
+    'jquery': 'jquery/jquery',
+    'jquery.cookie': 'js-cookie/js.cookie',
+    'js-cookie': 'js-cookie/js.cookie'
+  }
+});
+
+// @ts-ignore
+require(["Plaisio/PageDecorator/CorePageDecorator"]);
---
+++ www/js/Plaisio/PageDecorator/CorePageDecorator.ts
@@ -0,0 +1,56 @@
+import {Form} from "../Form/Form";
+import {OverviewTablePackage as OverviewTable} from "../Table/OverviewTablePackage";
+import {InputTable} from "./InputTable";
+import * as $ from 'jquery';
+
+export class CorePageDecorator
+{
+  //--------------------------------------------------------------------------------------------------------------------
+  public static init()
+  {
+    $('form').on('submit', InputTable.setCsrfValue);
+    Form.registerForm('form');
+    InputTable.registerTable('form');
+
+    // Register all overview tables.
+    let mq = window.matchMedia("only screen and (max-width: 40em)");
+    OverviewTable.registerTable('table.overview-table', mq);
+
+    // Install event handler for displaying the row count of am overview table.
+    $('body').on(OverviewTable.FILTERING_ENDED, CorePageDecorator.overviewTableRowCount)
+
+    if (window.hasOwnProperty('php_plaisio_inline_js'))
+    {
+      // @ts-ignore
+      eval(php_plaisio_inline_js);
+    }
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Updates the row count of an overview table.
+   *
+   * @param event The OverviewTable.FILTERING_ENDED event.
+   */
+  public static overviewTableRowCount(event): void
+  {
+    let $table = $(event.target);
+
+    let all = $table.children('tbody').children('tr').length;
+    let visible = $table.children('tbody').children('tr:visible').length;
+
+    if (all === visible)
+    {
+      $table.find('span.row_count').text(all);
+    }
+    else
+    {
+      $table.find('span.row_count').text(visible + '/' + all);
+    }
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+CorePageDecorator.init();
